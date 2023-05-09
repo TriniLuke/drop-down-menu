75d6f073eae6d6845fc7590b13287b857d4222bd
Luke Liverpool <lliverpool86@gmail.com>
2023-05-09 12:31:22 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # drop-down-menu
+
+I used the usual suspects to create this drop down menu "JavaScript". I had to do this as an exercise for The Odin Project and I am supposed to make it resusable. This is the reason why I used classes to create them so that it can be reused anywhere. Feel free to use.
+
+MIT License.
+
+<a href="">
---
+++ menu.js
@@ -0,0 +1,19 @@
+export default class DropDownMenu {
+  constructor(elem, link) {
+    this.elem = elem;
+    this.link = link;
+  }
+
+  hideAndDisplay() {
+    this.link.addEventListener("click", () => {
+      this.elem.style.visibility === "visible" ? this.elem.style.visibility = "hidden" : this.elem.style.visibility = "visible";
+    });
+  }
+}
+
+const menu = document.getElementById('drop-down-menu');
+const btn = document.getElementById('drop-down-menu-btn');
+
+const dropDownMenu = new DropDownMenu(menu, btn);
+
+dropDownMenu.hideAndDisplay();
